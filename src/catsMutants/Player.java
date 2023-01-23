3bc74fc18b921b797011dfef7fb110b5b7347798
Tetiana Koltunova <qaengineer.tetiana.koltunova@gmail.com>
2023-01-23 15:00:08 +0200

    Added this project to git

---
+++ src/catsMutants/Player.java
@@ -0,0 +1,14 @@
+package catsMutants;
+
+public class Player extends Humanoid {
+
+    public Player(String name, int hp, int baseDamage) {
+
+        super(name, hp, baseDamage);
+    }
+
+    @Override
+    public void prepareRound() {
+
+    }
+}
